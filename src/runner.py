f46f6c908d5bf43667bb409a56d1ce7182ec54f8
bugsancho <bugsancho@gmail.com>
2017-11-10 22:14:23 +0200

    Redoing the startup process so that running multiple spiders in parallel is easier

--- src/runner.py
+++ src/runner.py
@@ -1,2 +1,10 @@
-from scrapy.cmdline import execute
-execute(['scrapy','crawl', 'emag', "-a", "search_term=tp link c1200"])
+import scrapy
+from scrapy.crawler import CrawlerProcess
+from PriceComparer.spiders.emag import EmagSpider
+from PriceComparer.spiders.jar import JarSpider
+
+crawl_arguments = {'search_term': 'tp link c1200'}
+process = CrawlerProcess()
+process.crawl(EmagSpider, **crawl_arguments)
+process.crawl(JarSpider, **crawl_arguments)
+process.start()  # the script will block here until all crawling jobs are finished
