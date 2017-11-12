91b5960d1977dbeca26bd152af00cdf302ea21bd
bugsancho <bugsancho@gmail.com>
2017-11-12 23:13:28 +0200

    Implementing PC Store spider

--- src/PriceComparer/spiders/pcstore.py
+++ src/PriceComparer/spiders/pcstore.py
@@ -1,10 +1,46 @@
 import scrapy
+import urllib
+from ..product_offer import ProductOffer
 
 
 class PcstoreSpider(scrapy.Spider):
     name = 'pcstore'
     allowed_domains = ['pcstore.bg']
-    start_urls = ['https://www.pcstore.bg/']
+    search_page_url_template = 'https://www.pcstore.bg/bg/catalogsearch/result/?q=%s'
+
+    def __init__(self, search_term=None, *args, **kwargs):
+        super(PcstoreSpider, self).__init__(*args, **kwargs)
+        self.search_term = search_term
+
+    def start_requests(self):
+        url_escaped_search_term = urllib.parse.quote(self.search_term)
+        yield scrapy.Request(PcstoreSpider.search_page_url_template % url_escaped_search_term)
 
     def parse(self, response):
-        pass
+        product_container = response.css('.products-grid .item.first')
+
+        product_name = product_container.css(
+            '.product-name > a::attr(title)').extract_first()
+        url = product_container.css(
+            '.product-name > a::attr(href)').extract_first()
+
+        discounted_price_parts = product_container.css(
+            '.price-box .special-price .price').xpath('.//text()').extract()[:2]
+        discounted_price = ''.join(discounted_price_parts)
+
+        original_price_parts = product_container.css(
+            '.price-box .old-price .price').xpath('.//text()').extract()[:2]
+        original_price = ''.join(original_price_parts)
+
+        regular_price_parts = product_container.css(
+            '.price-box .regular-price .price').xpath('.//text()').extract()[:2]
+        regular_price = ''.join(regular_price_parts)
+
+        product_offer = ProductOffer(
+            retailer="pcstore",
+            url=url,
+            name=product_name,
+            price=original_price or regular_price,
+            discounted_price=discounted_price or None)
+
+        yield product_offer
--- src/runner.py
+++ src/runner.py
@@ -4,9 +4,11 @@ from scrapy.utils.project import get_project_settings
 
 from PriceComparer.spiders.emag import EmagSpider
 from PriceComparer.spiders.jar import JarSpider
+from PriceComparer.spiders.pcstore import PcstoreSpider
 
-crawl_arguments = {'search_term': 'tp link c1200'}
+crawl_arguments = {'search_term': 'tp link wr840n'}
 process = CrawlerProcess(get_project_settings())
 process.crawl(EmagSpider, **crawl_arguments)
 process.crawl(JarSpider, **crawl_arguments)
+process.crawl(PcstoreSpider, **crawl_arguments)
 process.start()  # the script will block here until all crawling jobs are finished
